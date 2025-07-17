c2e05e56d8ea339588cd36a7a4d7552c647bcc35
Ajay Vyas <ajay@sone4.no>
2025-07-17 12:07:18 +0530

    Simple use case for outbound call

---
+++ TwilioOutboundCallDemo/TwilioOutboundCallDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+Console.WriteLine("Hello, World!");
+
+//replace with your actual Twilio credentials
+const string accountSid = "AVV_SID";
+const string authToken = "auth_Token";
+
+TwilioClient.Init(accountSid, authToken);
+
+// replace with your Twilio phone number must start with +
+var from = new PhoneNumber("+91*****35878");
+
+// replace with your verified recipient phone number (yours, for testing)
+var to = new PhoneNumber("+91*****35878");
+
+// TwiML Bin url you created earlier
+var twimlUrl = new Uri("https://handler.twilio.com/twiml/twiMLId");
+
+var call = CallResource.Create(
+    to: to,
+    from: from,
+    url: twimlUrl
+);
+
+Console.WriteLine($"Call initiated! Call SID: {call.Sid}");
